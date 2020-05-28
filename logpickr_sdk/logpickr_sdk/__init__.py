f0cc32ffd9eef34de3ae9a83910d31b369021692
sioneptune <sioneptune@gmail.com>
2020-05-28 15:30:05 +0200

    feat(sdk): importing the module now succesfully imports all classes in top-level namespace

---
+++ logpickr_sdk/logpickr_sdk/__init__.py
@@ -0,0 +1,4 @@
+from logpickr_sdk.workgroup import Workgroup
+from logpickr_sdk.graph import Graph, Vertex, Edge
+from logpickr_sdk.project import Project
+print(f'Invoking __init__.py for {__name__}')
