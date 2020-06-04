7a39f2784333ac927d7b986eae5a96912c0b6a03
sioneptune <sioneptune@gmail.com>
2020-06-04 11:40:43 +0200

    refactor(Graph): split the types of graph into graph and graphinstance, and did the same for vertex and edge

--- logpickr_sdk/logpickr_sdk/graph.py
+++ logpickr_sdk/logpickr_sdk/graph.py
@@ -1,15 +1,14 @@
 import json
 
+
 class Graph:
     """A graph from a Logpickr project
     """
 
-    def __init__(self, id, vertices, edges, rework_total=-1, concurrency_rate=-1):
+    def __init__(self, id, vertices, edges):
         self.project_id = id
         self.vertices = vertices
         self.edges = edges
-        self.rework_total = rework_total
-        self.concurrency_rate = concurrency_rate
 
     @staticmethod
     def from_json(project_id, jsstring):
@@ -29,13 +28,27 @@ class Graph:
         return Graph(project_id, vertices, edges)
 
 
+class GraphInstance(Graph):
+
+    def __init__(self, id, vertices, edges, rework_total, concurrency_rate):
+        super().__init__(id, vertices, edges)
+        self.rework_total = rework_total
+        self.concurrency_rate = concurrency_rate
+
+
 class Vertex:
     """Vertex of a Logpicker Graph
     """
 
-    def __init__(self, id, name, event_instance=-1, concurrent_vertices=None):
+    def __init__(self, id, name):
         self.id = id
         self.name = name
+
+
+class VertexInstance(Vertex):
+
+    def __init__(self, id, name, event_instance, concurrent_vertices):
+        super().__init__(id, name)
         self.event_instance = event_instance
         self.concurrent_vertices = concurrent_vertices
 
@@ -44,8 +57,14 @@ class Edge:
     """Edge between two vertices of a Logpickr graph
     """
 
-    def __init__(self, id, source, destination, concurrent_edges=None):
+    def __init__(self, id, source, destination):
         self.id = id
         self.source = source
         self.destination = destination
+
+
+class EdgeInstance(Edge):
+
+    def __init__(self, id, source, destination, concurrent_edges=None):
+        super().__init__(id, source, destination)
         self.concurrent_edges = concurrent_edges
