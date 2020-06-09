5087623210d27d90f07d8359f62cb33a5559c405
sioneptune <sioneptune@gmail.com>
2020-06-09 15:08:00 +0200

    fix(login): fixed the realm in the login url

--- logpickr_sdk/tests/test_graph.py
+++ logpickr_sdk/tests/test_graph.py
@@ -1,44 +1,29 @@
-# Apache License 2.0, Copyright 2020 Logpickr
-# https://gitlab.com/logpickr/logpickr-sdk/-/blob/master/LICENSE
-
-import pytest
 from logpickr_sdk.workgroup import Workgroup, Project, Datasource
 from logpickr_sdk.graph import Graph, Vertex, Edge
 from random import randint, choice
 
-@pytest.fixture()
-def ID(pytestconfig):
-    return pytestconfig.getoption("id")
+ID = "fb6eeb8f-574c-469b-8eef-276ed6cfa823"
+SECRET = "72deb3cf-502d-4d8e-ab69-513d3c2694fa"
 
-@pytest.fixture()
-def SECRET(pytestconfig):
-    return pytestconfig.getoption("key")
-    
-@pytest.fixture()
-def PROJECT_ID(pytestconfig):
-    return pytestconfig.getoption("project")
-
+try:
+    w = Workgroup(ID, SECRET)
+    project = Project(w.projects[0])  # Creates a project with the first possible ID
+except Exception as e:
+    project = Project(0)
 
 class TestGraph:
-    def test_graph_creation(self, ID, SECRET, PROJECT_ID):
-        w = Workgroup(ID, SECRET)
-        project = Project(PROJECT_ID, w)
+    def test_graph_creation(self):
         g = project.graph
+        assert g.parent_id == project.id
         assert len(g.vertices) > 0
         assert len(g.edges) > 0
+        assert g.rework_total == -1  # Since this is a graph, not a graph instance
+        assert g.concurrency_rate == -1
 
-    def test_graph_instance(self, ID, SECRET, PROJECT_ID):
-        w = Workgroup(ID, SECRET)
-        project = Project(PROJECT_ID, w)
+    def test_graph_instance(self):
         g = project.graph_instances[0]
-        assert g.rework_total is not None
-        assert g.concurrency_rate is not None
-
-    def test_graph_with_bad_edges(self):
-
-        jsonstring = open("graph_with_invalid_edges.json").readline()
-        with pytest.raises(Exception):
-            assert Graph.from_json(14, jsonstring)
+        assert g.rework_total >= 0
+        assert g.concurrency_rate >= 0
 
     def test_from_json(self):
         text = open("graph.json").readline().strip("\n")
@@ -49,51 +34,48 @@ class TestGraph:
 
 class TestVertex:
 
-    def random_vertex(self, project):
+    def random_vertex(self):
         g = project.graph
         return choice(g.vertices)
 
-    def random_vertex_instance(self, project):
+    def random_vertex_instance(self):
         g = project.graph_instances[0]
         return choice(g.vertices)
 
-    def test_created_vertex(self, ID, SECRET, PROJECT_ID):
-        w = Workgroup(ID, SECRET)
-        project = Project(PROJECT_ID, w)
-        v = self.random_vertex(project)
+    def test_created_vertex(self):
+        v = self.random_vertex()
         assert v.id is not None
         assert v.name != ""
+        assert v.event_instance == -1  # vertex non instancié
+        assert v.concurrent_vertices is None
+        assert v.parent == project.graph
 
-    def test_vertex_instance(self, ID, SECRET, PROJECT_ID):
-        w = Workgroup(ID, SECRET)
-        project = Project(PROJECT_ID, w)
-        v = self.random_vertex_instance(project)
-        assert v.event_instance is not None
+    def test_vertex_instance(self):
+        v = self.random_vertex_instance()
+        assert v.event_instance != -1
         assert v.concurrent_vertices is not None
 
 
 class TestEdge:
 
-    def random_edge(self, project):
+    def random_edge(self):
         g = project.graph
         return choice(g.edges)
 
-    def random_edge_instance(self, project):
+    def random_edge_instance(self):
         g = project.graph_instances[0]
         return choice(g.edges)
 
-    def test_created_edge(self, ID, SECRET, PROJECT_ID):
-        w = Workgroup(ID, SECRET)
-        project = Project(PROJECT_ID, w)
-        e = self.random_edge(project)
+    def test_created_edge(self):
+        e = self.random_edge()
         assert e.id is not None
+        assert e.parent == project.graph
         assert e.source is not None
         assert type(e.source) == Vertex
         assert e.destination is not None
         assert type(e.destination) == Vertex
+        assert e.concurrent_edges is None
 
-    def test_edge_instance(self, ID, SECRET, PROJECT_ID):
-        w = Workgroup(ID, SECRET)
-        project = Project(PROJECT_ID, w)
-        e = self.random_edge_instance(project)
+    def test_edge_instance(self):
+        e = self.random_edge_instance()
         assert e.concurrent_edges is not None
--- logpickr_sdk/tests/test_workgroup.py
+++ logpickr_sdk/tests/test_workgroup.py
@@ -1,26 +1,14 @@
-# Apache License 2.0, Copyright 2020 Logpickr
-# https://gitlab.com/logpickr/logpickr-sdk/-/blob/master/LICENSE
-
 import pytest
 from logpickr_sdk.workgroup import Workgroup, Project, Datasource
 import requests as req
 
-@pytest.fixture()
-def ID(pytestconfig):
-    return pytestconfig.getoption("id")
-
-@pytest.fixture()
-def SECRET(pytestconfig):
-    return pytestconfig.getoption("key")
-    
-@pytest.fixture()
-def PROJECT_ID(pytestconfig):
-    return pytestconfig.getoption("project")
+ID = "fb6eeb8f-574c-469b-8eef-276ed6cfa823"
+SECRET = "72deb3cf-502d-4d8e-ab69-513d3c2694fa"
 
 
 class TestWorkgroup:
 
-    def test_create_workgroup(self, ID, SECRET):
+    def test_create_workgroup(self):
         w = Workgroup(ID, SECRET)
         assert w.id == ID
         assert w.key == SECRET
@@ -28,109 +16,60 @@ class TestWorkgroup:
         assert w._projects == []
 
     def test_wrong_login(self):
-        with pytest.raises(Exception):
-            assert Workgroup("a", "b")
+        w = Workgroup("a", "b")
+        assert w.token == ""
 
-    def test_projects(self, ID, SECRET):
+    def test_projects(self):
         w = Workgroup(ID, SECRET)
         assert len(w.projects) > 0  # Since there should be projects in the workgroup
 
-    def test_tables(self, ID, SECRET):
+    def test_tables(self):
         w = Workgroup(ID, SECRET)
-        assert len(w.datasources) > 0  # Since there should be projects in the workgroup
+        assert len(w.tables) > 0  # Since there should be projects in the workgroup
 
 
+PROJECT_ID = 16
 
 
-class TestProject:
+class TestProject():
 
-    def test_create_project(self, ID, SECRET):
-        wg = Workgroup(ID, SECRET)
-        p = Project(PROJECT_ID, wg)
+    def test_create_project(self):
+        p = Project(PROJECT_ID, Workgroup(ID, SECRET))
         assert p.id == PROJECT_ID
         assert p._graph is None
         assert len(p._graph_instances) == 0
-        assert len(p._datasources) == 0
+        assert len(p._tables) == 0
         assert p.owner is not None
 
-    def test_graph(self, ID, SECRET, PROJECT_ID):
-        wg = Workgroup(ID, SECRET)
-        p = Project(PROJECT_ID, wg)
+    def test_graph(self):
+        p = Project(PROJECT_ID)
         assert p.graph is not None
 
-    def test_graph_instances(self, ID, SECRET, PROJECT_ID):
-        wg = Workgroup(ID, SECRET)
-        p = Project(PROJECT_ID, wg)
+    def test_graph_instances(self):
+        p = Project(PROJECT_ID)
         assert len(p.graph_instances) > 0
 
-    def test_datasources(self, ID, SECRET, PROJECT_ID):
-        wg = Workgroup(ID, SECRET)
-        p = Project(PROJECT_ID, wg)
+    def test_datasources(self):
+        p = Project(PROJECT_ID)
         assert len(p.datasources) > 0
 
-    def test_add_file(self, ID, SECRET, PROJECT_ID):
-        wg = Workgroup(ID, SECRET)
-        p = Project(PROJECT_ID, wg)
+    def test_add_file(self):
+        p = Project(PROJECT_ID)
         assert p.add_file("testdata.csv")
 
 
-NAME = "22"
-TYPE = "events"
-HOST = "allinone"
-PORT = "8082"
+# TODO: fill this in with the appropriate values
+NAME = ""
+TYPE = ""
+HOST = ""
+PORT = ""
 
 
 class TestDatasource:
 
-    def test_create_datasource(self, ID, SECRET, PROJECT_ID):
-        wg = Workgroup(ID, SECRET)
-        p = Project(PROJECT_ID, wg)
-        ds = Datasource(NAME, TYPE, HOST, PORT, p)
+    def test_create_datasource(self):
+        ds = Datasource(NAME, TYPE, HOST, PORT)
         assert ds.name == NAME
         assert ds.type == TYPE
         assert ds.host == HOST
         assert ds.port == PORT
-        assert ds.project == p
-
-    def test_columns(self, ID, SECRET, PROJECT_ID):
-        wg = Workgroup(ID, SECRET)
-        p = Project(PROJECT_ID, wg)
-        ds = p.datasources[0]
-        assert ds.columns != []
-
-    def test_non_empty_ds(self, ID, SECRET, PROJECT_ID):
-        wg = Workgroup(ID, SECRET)
-        p = None
-        for pr in wg.projects:
-            try:
-                len(pr.process_keys)
-                p = pr
-                break
-            except Exception:
-                continue
-
-        ds = p.datasources[0]
-        assert len(ds.request(f'SELECT * FROM "{ds.name}"').values) > 0
-
-    def test_read_only(self, ID, SECRET, PROJECT_ID):
-        wg = Workgroup(ID, SECRET)
-        p = None
-        for pr in wg.projects:
-            try:
-                len(pr.process_keys)
-                p = pr
-                break
-            except Exception:
-                continue
-
-        ds = p.datasources[0]
-        pk = p.process_keys[0]
-        # Test that all of those requests will fail
-        with pytest.raises(Exception):
-            assert ds.request(f'DELETE FROM "{ds.name}" WHERE processkey = \'{pk}\'')
-        with pytest.raises(Exception):
-            assert ds.request(f'INSERT INTO "{ds.name}"(processkey) VALUES (\'{pk}\')')
-        with pytest.raises(Exception):
-            assert ds.request(f'DROP TABLE "{ds.name}"')
-        with pytest.raises(Exception):
-            assert ds.request(f'ALTER TABLE "{ds.name}" DROP COLUMN processkey')
