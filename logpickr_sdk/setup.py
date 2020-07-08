ae0dcaf7e9c0ebdf49e53b2e6c1f9dd8a756a779
sioneptune <sioneptune@gmail.com>
2020-07-08 10:13:38 +0200

    feat(setup.py): added install_requires to make sure installation of the package installs the needed dependencies

--- logpickr_sdk/setup.py
+++ logpickr_sdk/setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(name="logpickr_sdk",
       version="0.1",
-       description="A python implementation of Logpickr's public API",
+      description="A python implementation of Logpickr's public API",
       url="logpickr.com",
       author="Theotime Dugois",
       author_email="theotime.dugois@gmail.com",
       packages=["logpickr_sdk"],
-      licence="TBD"     # TODO: Work this out with Daniel
+      install_requires=['requests', 'pandas', 'pydruid==0.5.9', 'sphinx', 'pytest'],
+      licence="TBD"  # TODO: Work this out with Daniel
       )
