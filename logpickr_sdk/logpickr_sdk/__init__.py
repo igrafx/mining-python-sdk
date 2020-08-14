dad9860ee755a97de00713b9ce9bedd95808e199
sioneptune <sioneptune@gmail.com>
2020-08-14 14:05:20 +0200

    docs(all): added a whoooole bunch of docstrings and specified the parameters

--- logpickr_sdk/logpickr_sdk/__init__.py
+++ logpickr_sdk/logpickr_sdk/__init__.py
@@ -1,4 +1,4 @@
 # Apache License 2.0, Copyright 2020 Logpickr
 # https://gitlab.com/logpickr/logpickr-sdk/-/blob/master/LICENSE
-from logpickr_sdk.workgroup import Workgroup, Project, Datasource, set_API_URL, set_AUTH_URL
+from logpickr_sdk.workgroup import Workgroup, Project, Datasource, set_api_url, set_auth_url
 from logpickr_sdk.graph import Graph, Vertex, Edge
