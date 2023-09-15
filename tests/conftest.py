032804178751f819303b075088a83b600f188929
Dhruv.Maulloo <dhruv.maulloo@igrafx.com>
2023-09-15 14:08:53 +0200

    feat: change path in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,8 @@
 # https://github.com/igrafx/mining-python-sdk/blob/dev/LICENSE
 
 import pytest
-from igrafx_mining_sdk.workgroup import Workgroup
+from mining-python-sdk.igrafx_mining_sdk.workgroup import Workgroup
+# from igrafx_mining_sdk.workgroup import Workgroup
 import os
 
 
